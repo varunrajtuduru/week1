40a7dd0e52ec83461479472a800be309fde3ee21
varunrajtuduru <55775990+varunrajtuduru@users.noreply.github.com>
2019-11-05 21:52:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using assessment5;
+
+namespace assessment5
+{
+    class Program
+    {
+        //Customer customer;
+        static void Main(string[] args)
+        {
+             Program p = new Program();
+            p.getCustomerDetail();
+            p.displaycustomerdet();
+            
+            Console.ReadLine();
+        }
+        public void getCustomerDetail()
+        {
+            Console.WriteLine("Enter the name,productid,supplierid,numberof products going to buy");
+            Customer custom = new Customer();
+            custom.customername = Console.ReadLine();
+            custom.productid = int.Parse(Console.ReadLine());
+            custom.supplierid = int.Parse(Console.ReadLine());
+           
+            custom.numberofproducts = int.Parse(Console.ReadLine());
+
+            int count = custom.InsertNewCustomer(custom);
+            if (count > 0)
+                Console.WriteLine("record inserted successfully");
+            else
+                Console.WriteLine("something went wrong");
+
+        }
+        public void displaycustomerdet()
+        {
+            Customer customer = new Customer();
+            customer.displaycustomer(customer);
+
+
+        }
+    }
+}
---
+++ customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace assessment5
+{
+
+
+    public class Customer
+    {
+        public int supplierid,productid,numberofproducts;
+        public string customername;
+        SqlCommand cmd = new SqlCommand();
+        SqlConnection conn = new SqlConnection(" data source=IN5CG9214Z62 ; database=assessment5 ; integrated security = true ");
+
+        public int InsertNewCustomer(Customer custom)
+        {
+
+            cmd.Connection = conn;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_InsertNewCustomer";
+
+            cmd.Parameters.AddWithValue("customername", custom.customername);
+            cmd.Parameters.AddWithValue("productid", custom.productid);
+            cmd.Parameters.AddWithValue("supplierid", custom.supplierid);
+           
+           
+            cmd.Parameters.AddWithValue("numberofproducts", custom.numberofproducts);
+         
+            conn.Open();
+            int rowcount = cmd.ExecuteNonQuery();
+            conn.Close();
+            return rowcount;
+        }
+        public void displaycustomer(Customer customer)
+        {
+            cmd.Connection = conn;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_display";
+            conn.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine($"{rdr[0]}\t\t{rdr[1]}\t\t{rdr[2]}\t\t{rdr[3]}\t\t{rdr[4]}");
+            }
+
+            conn.Close();
+
+        }
+
+    }
+} 
