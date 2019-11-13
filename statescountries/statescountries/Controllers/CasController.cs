28ce6800e03ac2aeb14e57798812cf6c899bdd7b
varunrajtuduru <tuduruvarunraj@gmail.com>
2019-11-14 00:18:15 +0530

    statedropdown

---
+++ statescountries/statescountries/Controllers/CasController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using statescountries.Models;
+
+namespace statescountries.Controllers
+{
+    public class CasController : Controller
+    {
+        statescountriesEntities db = new statescountriesEntities();
+        // GET: Cas
+        public ActionResult Index()
+        {
+            ViewBag.countrylist = new SelectList(GetCountryList(),"countriesid","country1");
+            return View();
+        }
+
+        public List<country> GetCountryList()
+        {
+            List<country> countries = db.countries.ToList();
+            return countries;
+        }
+
+        public ActionResult GetStateList(int cid)
+        {
+            List<state> selectlist = db.states.Where(x => x.countryid == cid).ToList();
+            ViewBag.slist = new SelectList(selectlist, "stateid", "state1");
+            return PartialView("displaystates");
+        }
+
+
+    }
+}
---
+++ statescountries/statescountries/Models/CascadingClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace statescountries.Models
+{
+    public class CascadingClass
+    {
+        public int cid { get; set; }
+        public int sid { get; set; }
+    }
+}
